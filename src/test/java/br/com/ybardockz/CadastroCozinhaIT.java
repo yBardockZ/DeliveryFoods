eff8c8a9a815d5cad82f3a3bbcc05ab55f68c6a3
Thalles <thalles_leopoldino@outlook.com>
2024-12-09 02:00:49 -0300

    Criando um método para fazer setup dos testes

--- src/test/java/br/com/ybardockz/CadastroCozinhaIT.java
+++ src/test/java/br/com/ybardockz/CadastroCozinhaIT.java
@@ -4,10 +4,12 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,11 +18,16 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinha";
+	}
+	
 	@Test
 	public void deveRetornar200_QuandoConsultarCozinhas() {
 		given()
-		.port(port)
-		.basePath("/cozinha")
 		.accept(ContentType.JSON)
 		.when()
 			.get()
@@ -33,8 +40,6 @@ public class CadastroCozinhaIT {
 	@Test
 	public void deveConterQuatroCozinhas_QuandoConsultarCozinhas() {
 		given()
-		.port(port)
-		.basePath("/cozinha")
 		.accept(ContentType.JSON)
 		.when()
 			.get()
