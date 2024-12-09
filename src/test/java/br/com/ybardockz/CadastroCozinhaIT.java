ab5316ebff4316e4c369b540cde5743715ce4f00
Thalles <thalles_leopoldino@outlook.com>
2024-12-09 01:52:12 -0300

    Validando corpo da resposta HTTP

--- src/test/java/br/com/ybardockz/CadastroCozinhaIT.java
+++ src/test/java/br/com/ybardockz/CadastroCozinhaIT.java
@@ -1,6 +1,8 @@
 package br.com.ybardockz;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +26,21 @@ public class CadastroCozinhaIT {
 			.get()
 		.then()
 			.statusCode(200);
+		
+		
+	}
+	
+	@Test
+	public void deveConterQuatroCozinhas_QuandoConsultarCozinhas() {
+		given()
+		.port(port)
+		.basePath("/cozinha")
+		.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(4))
+			.body("nome", hasItems("Tailandesa", "Indiana"));
 		
 		
 	}
