602fad96eb1c1819d49ad44a5b74c5f858b0b28a
Thalles <thalles_leopoldino@outlook.com>
2024-12-09 02:37:12 -0300

    Limpando e populando o banco de dados de teste

--- src/test/java/br/com/ybardockz/CadastroCozinhaIT.java
+++ src/test/java/br/com/ybardockz/CadastroCozinhaIT.java
@@ -6,23 +6,39 @@ import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
+import br.com.ybardockz.domain.model.Cozinha;
+import br.com.ybardockz.domain.repository.CozinhaRepository;
+import br.com.ybardockz.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@BeforeEach
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinha";
+		
+		databaseCleaner.clearTables();
+		
+		prepararDados();
 	}
 	
 	@Test
@@ -44,12 +60,30 @@ public class CadastroCozinhaIT {
 		.when()
 			.get()
 		.then()
-			.body("", hasSize(4))
+			.body("", hasSize(2))
 			.body("nome", hasItems("Tailandesa", "Indiana"));
 		
 		
 	}
-
-
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+		.body("{ \"nome\": \"Chinesa\" }")
+		.contentType(ContentType.JSON)
+		.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(201);
+				
+	}
+	
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha(null, "Tailandesa");
+		cozinhaRepository.save(cozinha1);
+		Cozinha cozinha2 = new Cozinha(null, "Americana");
+		cozinhaRepository.save(cozinha2);
+	}
 
 }
