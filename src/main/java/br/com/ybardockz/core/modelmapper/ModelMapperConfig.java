3845f7ba4072a824cb3d74eaa45cb8b17cabb936
Thalles <thalles_leopoldino@outlook.com>
2025-01-23 19:31:25 -0300

    Adicionando o endereço no modelo de representação de restaurante

--- src/main/java/br/com/ybardockz/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/br/com/ybardockz/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.ybardockz.api.model.domain.EnderecoModel;
+import br.com.ybardockz.domain.model.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-		return new ModelMapper();
+    	var modelMapper = new ModelMapper();
+    	
+    	var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class,
+    			EnderecoModel.class);
+    	
+    	enderecoToEnderecoModelTypeMap.addMapping(src -> src.getCidade().getEstado().getNome(),
+    			(dest, value) -> dest.getCidade().setEstado((String) value));
+    	
+		return modelMapper;
 	}
 
 }
