a9576c13ca4ca7aa7789102fe966fe2b528c880e
Thalles <thalles_leopoldino@outlook.com>
2025-01-31 16:17:06 -0300

    Limitando os campos retornados pela API com @JsonFilter

--- src/main/java/br/com/ybardockz/api/controller/PedidoController.java
+++ src/main/java/br/com/ybardockz/api/controller/PedidoController.java
@@ -2,15 +2,21 @@ package br.com.ybardockz.api.controller;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import br.com.ybardockz.api.model.assembler.PedidoInputDisassembler;
 import br.com.ybardockz.api.model.assembler.PedidoModelAssembler;
@@ -46,11 +52,35 @@ public class PedidoController {
 	private PedidoInputDisassembler pedidoInputDisassembler;
 	
 	@GetMapping
-	public List<PedidoResumoModel> listar() {
+	public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		
-		return pedidoResumoModelAssembler.toCollectionModel(pedidos);
+		List<PedidoResumoModel> pedidosResumoModel = 
+				pedidoResumoModelAssembler.toCollectionModel(pedidos);
+		
+		MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosResumoModel);
+		
+		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+		filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+		
+		if (StringUtils.isNotBlank(campos)) {
+			filterProvider.addFilter("pedidoFilter", 
+					SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+		}
+		
+		pedidosWrapper.setFilters(filterProvider);
+		
+		return pedidosWrapper;
+		
 	}
+	
+	
+// @GetMapping
+//	public List<PedidoResumoModel> listar() {
+//		List<Pedido> pedidos = pedidoRepository.findAll();
+//		
+//		return pedidoResumoModelAssembler.toCollectionModel(pedidos);
+//	}
 	
 	@GetMapping("/{pedidoCodigo}")
 	public PedidoModel buscarPorId(@PathVariable String pedidoCodigo) {
--- src/main/java/br/com/ybardockz/api/model/domain/PedidoResumoModel.java
+++ src/main/java/br/com/ybardockz/api/model/domain/PedidoResumoModel.java
@@ -3,10 +3,13 @@ package br.com.ybardockz.api.model.domain;
 import java.math.BigDecimal;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
 import br.com.ybardockz.domain.model.enums.StatusPedido;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonFilter("pedidoFilter")
 @Getter
 @Setter
 public class PedidoResumoModel {
