f7c6cc3e6c751171f5efc2dcab0d7ed379af7842
Thalles <thalles_leopoldino@outlook.com>
2025-02-11 00:41:30 -0300

    Configurando CORS globalmente

--- src/main/java/br/com/ybardockz/api/controller/RestauranteController.java
+++ src/main/java/br/com/ybardockz/api/controller/RestauranteController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +30,6 @@ import br.com.ybardockz.domain.repository.RestauranteRepository;
 import br.com.ybardockz.domain.service.CadastroRestauranteService;
 import jakarta.validation.Valid;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/restaurante")
 public class RestauranteController {
