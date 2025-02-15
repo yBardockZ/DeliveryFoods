44837b239bfff505e4732c3d467a1e7c6907e621
Thalles <thalles_leopoldino@outlook.com>
2025-02-15 00:32:20 -0300

    Escrevendo documentação do endpoint de Grupos

--- src/main/java/br/com/ybardockz/api/controller/GrupoController.java
+++ src/main/java/br/com/ybardockz/api/controller/GrupoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ybardockz.api.controller.openapi.GrupoControllerOpenApi;
 import br.com.ybardockz.api.model.assembler.GrupoInputDisassembler;
 import br.com.ybardockz.api.model.assembler.GrupoModelAssembler;
 import br.com.ybardockz.api.model.domain.GrupoModel;
@@ -25,7 +26,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/grupo")
-public class GrupoController {
+public class GrupoController implements GrupoControllerOpenApi {
 	
 	@Autowired
 	private GrupoRepository grupoRepository;
---
+++ src/main/java/br/com/ybardockz/api/controller/openapi/GrupoControllerOpenApi.java
@@ -0,0 +1,66 @@
+package br.com.ybardockz.api.controller.openapi;
+
+import java.util.List;
+
+import br.com.ybardockz.api.exceptionhandler.Problema;
+import br.com.ybardockz.api.model.domain.GrupoModel;
+import br.com.ybardockz.api.model.input.GrupoInput;
+import br.com.ybardockz.domain.model.Grupo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Grupos", description = "Gerencia os grupos")
+public interface GrupoControllerOpenApi {
+
+	@Operation(summary = "Lista os grupos")
+	public List<GrupoModel> listar();
+	
+	@Operation(summary = "Busca um grupo")
+	@ApiResponses({
+		@ApiResponse(responseCode = "404", description = "Grupo não encontrado",
+				content = @Content(mediaType = "application/json",
+						schema = @Schema(implementation = Problema.class))),
+		
+		@ApiResponse(responseCode = "400", description = "ID inválido",
+		content = @Content(mediaType = "application/json",
+				schema = @Schema(implementation = Problema.class)))
+	})
+	public GrupoModel buscarPorId(@Parameter(required = true, example = "1") 
+		Long grupoId);
+	
+	@Operation(summary = "Cadastra um grupo")
+	@ApiResponses({
+		@ApiResponse(responseCode = "201", description = "Grupo cadastrado",
+				content = @Content(mediaType = "application/json",
+						schema = @Schema(implementation = GrupoModel.class)))
+	})
+	public GrupoModel adicionar(GrupoInput grupoInput);
+	
+	@Operation(summary = "Atualiza um grupo")
+	@ApiResponses({
+		@ApiResponse(responseCode = "404", description = "Grupo não encontrado",
+				content = @Content(mediaType = "application/json",
+						schema = @Schema(implementation = Problema.class))),
+		
+		@ApiResponse(responseCode = "200", description = "GrupoAtualizado",
+		content = @Content(mediaType = "application/json",
+				schema = @Schema(implementation = Grupo.class)))
+	})
+	public GrupoModel atualizar(@Parameter(required = true, example = "1") Long grupoId, GrupoInput grupoInput);
+	
+	@ApiResponses({
+		@ApiResponse(responseCode = "204", description = "Grupo excluido"),
+		
+		@ApiResponse(responseCode = "404", description = "Grupo não encontrado",
+		content = @Content(mediaType = "application/json",
+				schema = @Schema(implementation = Grupo.class)))
+	})
+	@Operation(summary = "Deleta um grupo")
+	public void deletar(@Parameter(required = true, example = "1") Long grupoId);
+
+}
