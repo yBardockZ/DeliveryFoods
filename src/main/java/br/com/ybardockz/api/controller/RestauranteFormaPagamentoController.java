4c8191ac657c8e2f748c02bc70655bfeeae32c5f
Thalles <thalles_leopoldino@outlook.com>
2025-02-18 02:09:18 -0300

    Escrevendo documentação do endpoint de associação entre restaurante e formas de pagamento

--- src/main/java/br/com/ybardockz/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/br/com/ybardockz/api/controller/RestauranteFormaPagamentoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ybardockz.api.model.assembler.FormaPagamentoModelAssembler;
 import br.com.ybardockz.api.model.domain.FormaPagamentoModel;
+import br.com.ybardockz.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
 import br.com.ybardockz.domain.model.Restaurante;
 import br.com.ybardockz.domain.service.CadastroRestauranteService;
 
 @RestController
-@RequestMapping("/restaurante/{restauranteId}/forma-pagamento")
-public class RestauranteFormaPagamentoController {
+@RequestMapping(path = "/restaurante/{restauranteId}/forma-pagamento",
+		produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 	
 	@Autowired
 	private CadastroRestauranteService restauranteService;
