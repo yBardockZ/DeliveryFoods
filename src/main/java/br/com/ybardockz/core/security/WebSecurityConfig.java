62f65e578eaea3113f4ab578bc272fe636c790cf
Thalles <thalles_leopoldino@outlook.com>
2025-04-07 16:30:14 -0300

    Configurando Resource Server para jwt assinado com chave simétrica

--- src/main/java/br/com/ybardockz/core/security/WebSecurityConfig.java
+++ src/main/java/br/com/ybardockz/core/security/WebSecurityConfig.java
@@ -1,11 +1,18 @@
 package br.com.ybardockz.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -20,13 +27,35 @@ public class WebSecurityConfig {
 				authorize
 					.anyRequest().authenticated()
 					)
-			.cors(cors -> cors.disable())
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			.oauth2ResourceServer(resource -> 
-					resource.opaqueToken(Customizer.withDefaults())
+					resource.jwt(Customizer.withDefaults())
 					);
 		
 		return http.build();
 		
 	}
+	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8082");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+	
+	@Bean
+	JwtDecoder jwtDecoder() {
+		var secretKey = new SecretKeySpec("supkdfjauqifhjdkfgioiweqasdfgmnc".getBytes(),
+				"HmacSHA256");
+		
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
+	}
+	
 	
 }
